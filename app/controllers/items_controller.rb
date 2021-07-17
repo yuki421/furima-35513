fb6eae5c0cc21c75090edba1a3ab018269af7e60
yuki421 <teradayuki421@gmail.com>
2021-07-17 17:42:46 +0900

    保存できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,22 @@ class ItemsController < ApplicationController
   end
   
   def new
-    @Item = Item.new
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :item_status_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
   
 end
