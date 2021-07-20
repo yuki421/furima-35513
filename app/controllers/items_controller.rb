29464d291badb250fea650f80bb91fdb3a44113b
yuki421 <teradayuki421@gmail.com>
2021-07-20 16:47:24 +0900

    レビュー修正2回目（追加）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new, :create #:destroy
+  before_action :authenticate_user!, only: [:new, :create] #:destroy
 
   def index
   end
