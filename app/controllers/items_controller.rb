cb3cf9c9384b8d6956d64b86c47c812b0c349eeb
yuki421 <teradayuki421@gmail.com>
2021-07-24 19:46:57 +0900

    編集情報が保存できる、バリデーション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create] #:destroy
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update] #:destroy
 
   def index
     @items = Item.order("created_at DESC")
@@ -24,10 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 
   def update
-    
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
