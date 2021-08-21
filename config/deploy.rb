ebad4344e5f91857c48dfb7efcaa08072948af5c
yuki421 <teradayuki421@gmail.com>
2021-08-21 20:55:40 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.16.0'
 set :application, 'furima-35513'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'you-key421/furima-35513.git'
+set :repo_url,  'yuki421/furima-35513.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
